2d4de0d35256e01c49df75d2ae18cc0addbb0a2c
Nykidxxx <yao.ni@yahoo.com>
2017-03-08 19:37:45 -0500

    Multi-Column Listview is successfully built with dummy data.
    Next step is to grab data from Database.

---
+++ app/src/main/java/com/nykidxxx/pfv2/Constants.java
@@ -0,0 +1,11 @@
+package com.nykidxxx.pfv2;
+
+/**
+ * Created by Nykidxxx on 3/8/2017.
+ */
+
+public class Constants {
+    public static final String FIRST_COLUMN = "First";
+    public static final String SECOND_COLUMN = "Second";
+    public static final String THIRD_COLUMN = "Third";
+}
